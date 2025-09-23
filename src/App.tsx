0858051f73099c87f17eefba7294033d4404c64a
reyhermanses <rey.undot@gmail.com>
2025-09-23 16:29:21 +0700

    fix vercel deployment - simplified config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 function App() {
   return (
-    <div style={{
-      height: '100vh',
-      background: '#dc2626',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: 'white',
-      fontSize: '32px',
-      fontWeight: 'bold'
-    }}>
-      <div>BOGA GROUP - TEST</div>
+    <div>
+      BOGA GROUP - TEST
     </div>
   )
 }
