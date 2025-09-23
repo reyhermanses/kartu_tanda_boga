7bede0bc42da46bc04acd57079c309b7720e391d
reyhermanses <rey.undot@gmail.com>
2025-09-23 16:46:12 +0700

    test with pure HTML - no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <div>
+    <div style={{
+      height: '100vh',
+      background: '#dc2626',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: '32px',
+      fontWeight: 'bold'
+    }}>
       BOGA GROUP - TEST
     </div>
   )
